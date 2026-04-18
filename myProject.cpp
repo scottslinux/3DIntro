5c776319c739041b6e2a86cae03cb6cfa584e15f
Scott Sher <scottslinux@gmail.com>
2026-04-17 20:40:17 -0700

    movement and skybox

--- myProject.cpp
+++ myProject.cpp
@@ -12,6 +12,8 @@ float theta=0;  //initial vector direction
 Vector3 forward={0,0,1.0f}; //initial forward
 
 
+
+
 //----------------------------------------------------------------------------------------
 Vector3 delta(Vector3 origpos, Vector3 newpos)
 {
@@ -35,12 +37,17 @@ Vector3 newforward(Vector3 &first, float angle)
 
 }
 //----------------------------------------------------------------------------------------
-void miniMap()
+void miniMap(Vector3 pos,Vector3 target)
 {
+    //center point 800,250
     DrawRectangleLines(650,100,300,300,GREEN);
 
     DrawLine(650+150,100,650+150,100+300,GREEN);
     DrawLine(650,100+150,650+300,100+150,GREEN);
+
+
+    DrawCircle(pos.x+800,pos.z+250,4,RED);
+    DrawLine(pos.x+800,pos.z+250,(target.x*3)+800,(target.z*3)+250,BLUE);
 
 
 
@@ -55,6 +62,8 @@ int main()
     InitWindow(1000,1000,"3D-Attempt");
     SetTargetFPS(60);
 
+    Texture2D sky=LoadTexture("./resources/sky.png");
+
     Camera3D camera = { 0 };
 
     camera.position = (Vector3){ 0.0f, 10.0f, 1.0f }; // Position (where the camera is in the world)
@@ -66,7 +75,13 @@ int main()
     oldpos=camera.position; //start with initial pos
     Vector3 forward={0,0,1.0f};
     Vector3 right={1.0f,0,0};
-    
+
+    float dx=0, dz=0;
+
+    Mesh orb=GenMeshSphere(400,50,50);
+    Model earth=LoadModelFromMesh(orb);
+    earth.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture=sky;
+
 
     while(!WindowShouldClose())
     {
@@ -76,22 +91,24 @@ int main()
             //      front and back movement
 
             float joy1=GetGamepadAxisMovement(2,1);
+        
 
             if(joy1<(-.1))
                 {
-                    camera.position=Vector3Add(camera.position,forward);
-                    
+                    dz=-1*joy1;
+                    camera.position.z=camera.position.z+(cos(theta)*dz);
+                    camera.position.x=camera.position.x+(sin(theta)*dz);
 
-
-
-                    
-                    
                 }
-            if(joy1>(.1))
-                {
-                    
-                    
-                }
+                else
+                    if(joy1>(.1))
+                    {
+                        dz=1*joy1;
+                        camera.position.z=camera.position.z-(cos(theta)*dz);
+                        camera.position.x=camera.position.x-(sin(theta)*dz);
+
+                    }
+                    else dz=0;  //no forward movement
 
             //======================================
             //      lateral joy---> rotation of view
@@ -104,8 +121,7 @@ int main()
                     if(theta>2*PI) theta=0;
                     
                     theta+=PI/180.0f;       //add 1 degree
-                    newforward(camera.target, theta);
-                    forward=camera.target;          //set the addition vector to the place you are looking
+                    
 
                     
                 }
@@ -114,51 +130,69 @@ int main()
                     if(theta<-2*PI) theta=0;
                    
                     theta-=PI/180.0f;       //subtract 1 degree
-                    newforward(camera.target, theta);   //by reference changes target according to angle
-                    forward=camera.target;
-
-
                     
+   
                 }
             //======================================
-            system("clear");
-            cout<<"X: "<<camera.position.x<<" Y: "<<camera.position.y<<" Z: "<<camera.position.z<<endl;
-            cout<<"X: "<<forward.x<<" Y: "<<forward.y<<" Z: "<<forward.z<<endl;
-            cout<<theta<<"  radians\n";
+            
+
+            
+            
+            
+            camera.position.y=2;
+            
+            
+
+            camera.target.x=camera.position.x+(sin(theta)*5);
+            camera.target.z=camera.position.z+(cos(theta)*5);
+            camera.target.y=2;
+
+
+
+
+
+            
 
             UpdateCamera(&camera, CAMERA_FREE);
 
-                        // Lock height
-            camera.position.y = 2.0f;
-            camera.target.y   = 2.0f;
+           
+
+            
 
             // Keep inside world
             float limit = 10.0f;
 
-            //camera.position.x = std::clamp(camera.position.x, -limit, limit);
-            //camera.position.z = std::clamp(camera.position.z, -limit, limit);
+            system("clear");
+            cout<<"Position--->X: "<<camera.position.x<<" Y: "<<camera.position.y<<" Z: "<<camera.position.z<<endl;
+            cout<<"View------->X: "<<camera.target.x<<" Y: "<<camera.target.y<<" Z: "<<camera.position.z<<endl;
+            cout<<theta<<"  radians\n";
 
 
 
-            ClearBackground(BLACK);
+            ClearBackground(WHITE);
             DrawText("  3D \n\n\nWorld",350,80,50,WHITE);
 
             BeginMode3D(camera);
 
-                //DrawPlane((Vector3){ 0.0f, 0.0f, 0.0f }, (Vector2){ 20.0f, 20.0f }, GRAY);
-                DrawGrid(100, 1.0f);
+                DrawPlane((Vector3){ 0.0f, 0.0f, 0.0f }, (Vector2){ 1000.0f, 1000.0f }, BLACK);
+                //DrawGrid(1000, 1.0f);
                 DrawCube((Vector3){ 50.0f, 2.0f, 50.0f }, 1.0f, 4.0f, 1.0f, GREEN);
                 DrawCube((Vector3){ 50.0f, 2.0f, -50.0f }, 1.0f, 4.0f, 1.0f, BLUE);
                 DrawCube((Vector3){ -50.0f, 2.0f, 50.0f }, 1.0f, 4.0f, 1.0f, RED);
                 DrawCube((Vector3){ -50.0f, 2.0f, -50.0f }, 1.0f, 4.0f, 1.0f, ORANGE);
 
-                //DrawSphere((Vector3){0,10,0},5,BLUE);
+
+                rlDisableBackfaceCulling();
+                    DrawModel(earth,{0,0,0},1.0f,WHITE);
+                rlEnableBackfaceCulling();
+                
+
 
               
 
             EndMode3D();
                 
-                miniMap();
+                miniMap(camera.position,camera.target);
 
         EndDrawing();
 
@@ -170,9 +204,3 @@ int main()
     
 }
 
-/*
-            if (IsKeyDown(KEY_W)) camera.position.z -= 0.1f;
-            if (IsKeyDown(KEY_S)) camera.position.z += 0.1f;
-            if (IsKeyDown(KEY_A)) camera.position.x -= 0.1f;
-            if (IsKeyDown(KEY_D)) camera.position.x += 0.1f;
-*/
